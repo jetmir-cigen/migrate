ad032a47aaea51d860e1a5b451e4fd60eea5391c
Jetmir Avdullahu <jetmir.avdullahu@cigen.me>
2023-04-12 18:01:19 +0200

    introducing modules path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DepartmentModule } from './department/department.module';
-import { UserModule } from './user/user.module';
-import { DepartmentModule } from './department/department.module';
+import { DepartmentModule } from './modules/department/department.module';
 
 @Module({
-  imports: [DepartmentModule, UserModule],
+  imports: [DepartmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/department/department.module.ts
+++ src/modules/department/department.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DepartmentService } from './department.service';
 import { DepartmentController } from './department.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [DepartmentController],
   providers: [DepartmentService],
 })
