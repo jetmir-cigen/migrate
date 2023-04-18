79d5e1dd96212ffea4292b65db801dec5cea3923
Dean Gite <dgite@ritech.co>
2023-04-18 22:54:09 +0200

    add guards

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Query, UseGuards } from '@nestjs/common';
 import { UserQueryDto } from './dto/user-query.dto';
 import { QueryBus } from '@nestjs/cqrs';
 import { FindUsersByFilterQuery } from './queries/find-users.query';
 import { UserResponseDto } from './dto/user-response.dto';
+import { AuthGuard } from '@/modules/auth/auth.guard';
+import { UserRoleGuard } from './user-role.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly queryBus: QueryBus) {}
 
-  @Get()
+  @Get('/')
+  @UseGuards(AuthGuard, UserRoleGuard(['ADMIN_USER']))
   async getAll(@Query() query: UserQueryDto): Promise<UserResponseDto> {
     const {
       filterByUsername,
