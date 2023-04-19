c21001581892d368715adb8946a1409536c2e367
Jetmir Avdullahu <jetmir.avdullahu@cigen.me>
2023-04-20 01:05:27 +0200

    Add swagger decorators for create and update department dto-s

--- src/modules/department/dto/update-department.dto.ts
+++ src/modules/department/dto/update-department.dto.ts
@@ -1,45 +1,70 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsNotEmpty,
   IsNumber,
   IsOptional,
   IsString,
-  Min,
 } from 'class-validator';
 
 export class UpdateDepartmentDto {
-  @IsNumber({
-    allowInfinity: false,
-    allowNaN: false,
-    maxDecimalPlaces: 0,
+  @ApiProperty({
+    example: 'Department of Engineering',
+    description: 'The name of the department',
   })
-  @Min(1)
-  id: number;
-
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    example: 123,
+    description: 'The code of the department',
+  })
   @IsNumber()
   @IsNotEmpty()
   code: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'The ID of the user who created the department',
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
   userId?: number;
 
+  @ApiProperty({
+    example: 2,
+    description: 'The ID of the deputy user of the department',
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
   deputyUserId?: number;
 
+  @ApiProperty({
+    example: 'Project X',
+    description: 'The project associated with the department',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   project?: string;
 
+  @ApiProperty({
+    example: true,
+    description: 'Whether the deputy user should receive emails',
+    required: false,
+  })
   @IsOptional()
   @IsBoolean()
   deputyMail?: boolean;
 
+  @ApiProperty({
+    example: false,
+    description: 'Whether the department is inactive',
+    required: false,
+  })
   @IsOptional()
   @IsBoolean()
   inactive?: boolean;
