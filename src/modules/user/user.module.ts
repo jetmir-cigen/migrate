9f11f34e55596f50fa463a8b0449d61042fae79b
Dean Gite <dgite@ritech.co>
2023-04-23 12:59:11 +0200

    add mailer module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,13 +1,13 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
+import { HttpModule } from '@nestjs/axios';
 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
 import { FindUsersByFilterQueryHandler } from './queries/find-users.query';
 import { UserGroupEntity } from './entities/user-group.entity';
-import { HttpModule } from '@nestjs/axios';
 import {
   CreateUserCommandHandler,
   DeleteUserCommandHandler,
