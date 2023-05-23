c88e054289832a2244f941b8be95fae9e587cf2c
megibegi <ms.magdalena.kozlowska@gmail.com>
2023-05-23 20:56:49 +0200

    Fix imports

--- src/modules/employee-consent/employee-consent.module.ts
+++ src/modules/employee-consent/employee-consent.module.ts
@@ -3,8 +3,15 @@ import { EmployeeConsentController } from './employee-consent.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeConsentEntity } from './entities/employee-consent.entity';
 import { EmployeeConsentCostObjectEntity } from '@/common/entities/employee-consent-cost-object.entity';
-import { CreateEmployeeConsentCommandHandler } from './employee-consent.command';
-import { GetEmployeeConsentsQuery } from './get-employee-consents.query';
+import {
+  CreateEmployeeConsentCommand,
+  CreateEmployeeConsentCommandHandler,
+} from './employee-consent.command';
+import {
+  GetEmployeeConsentsQuery,
+  GetEmployeeConsentsQueryHandler,
+} from './get-employee-consents.query';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
   imports: [
@@ -12,8 +19,14 @@ import { GetEmployeeConsentsQuery } from './get-employee-consents.query';
       EmployeeConsentEntity,
       EmployeeConsentCostObjectEntity,
     ]),
+    CqrsModule,
   ],
   controllers: [EmployeeConsentController],
-  providers: [CreateEmployeeConsentCommandHandler, GetEmployeeConsentsQuery],
+  providers: [
+    CreateEmployeeConsentCommandHandler,
+    CreateEmployeeConsentCommand,
+    GetEmployeeConsentsQuery,
+    GetEmployeeConsentsQueryHandler,
+  ],
 })
 export class EmployeeConsentModule {}
