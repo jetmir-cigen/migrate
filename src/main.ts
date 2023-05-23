e64ee84646ce63ebe834d7dc94ba9b4690c10f36
Jetmir Avdullahu <jetmir.avdullahu@cigen.me>
2023-05-23 16:07:25 +0200

    cors update

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   // Enable cors, so FE can access it.
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000', /.+vercel\.app/],
+    origin: ['http://localhost:3000', /\.vercel\.app$/],
     allowedHeaders: ['authorization', 'cookie', 'cookies', 'content-type'],
   });
 
