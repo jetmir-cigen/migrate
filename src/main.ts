2ca5fa48a08e08346ef26c2273492292e08683a9
Dean Gite <dean.gite@cigen.me>
2023-06-07 15:35:15 +0000

    Merged PR 394: AB#1134 add skytechcontrol
    AB#1134 add skytechcontrol
    
    Related work items: #1134

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   // Enable cors, so FE can access it.
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000', /.+vercel\.app/],
+    origin: ['http://localhost:3000', /.+vercel\.app/, /.+skytechcontrol\.io/],
     allowedHeaders: [
       'authorization',
       'cookie',
