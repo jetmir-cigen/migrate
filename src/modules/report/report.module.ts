cf08bbb673308d61735a18d02e7e3d73b8a9a414
Jetmir Avdullahu <jetmir.avdullahu@cigen.me>
2023-06-19 19:00:05 +0200

    AB#1096 remove unused entities

--- src/modules/report/report.module.ts
+++ src/modules/report/report.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ReportController } from './report.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrderDownPaymentEntity } from '@/modules/report/entities/order-downpayment.entity';
 import { CqrsModule } from '@nestjs/cqrs';
 import {
   GetSetupExportQueryHandler,
@@ -11,27 +10,10 @@ import {
 import { CustomerSetupExportSettingsEntity } from '@/modules/report/entities/customer-setup-export-settings.entity';
 import { SetupExportSettingsController } from '@/modules/report/setup-export-settings.controller';
 import { UpsertSetupExportSettingsCommandHandler } from '@/modules/report/commands/upsert-setup-export-settings.controller';
-import { CostObjectEntity } from '@/common/entities/cost-object.entity';
-import { DPOrderDownPaymentEntity } from './entities/device_policy.order_down_payment.entity';
-import { EOrderDownPaymentEntity } from './entities/ecom.order_down_payment.entity';
-import { DPOrderEntity } from './entities/device_policy.order.entity';
-import { DPProductEntity } from './entities/device_policy.product.entity';
-import { EOrderEntity } from './entities/ecom.order.entity';
-import { AAssetEntity } from './entities/assets.asset.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      OrderDownPaymentEntity,
-      CostObjectEntity,
-      CustomerSetupExportSettingsEntity,
-      EOrderDownPaymentEntity,
-      DPOrderDownPaymentEntity,
-      DPOrderEntity,
-      DPProductEntity,
-      EOrderEntity,
-      AAssetEntity,
-    ]),
+    TypeOrmModule.forFeature([CustomerSetupExportSettingsEntity]),
     CqrsModule,
   ],
   controllers: [ReportController, SetupExportSettingsController],
