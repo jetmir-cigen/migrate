f7f6b401ab6406a4e13bc1665881219452c54d25
Dean Gite <dean.gite@cigen.me>
2023-10-02 16:12:04 +0000

    Merged PR 661: get asset
    Related work items: #1492

--- src/modules/asset/asset.controller.ts
+++ src/modules/asset/asset.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiBody,
   ApiInternalServerErrorResponse,
+  ApiParam,
   ApiResponse,
 } from '@nestjs/swagger';
 import {
@@ -10,15 +11,19 @@ import {
   TextTemplateDto,
 } from '@/modules/text-template/dto';
 import { AuthUser } from '@/modules/auth/auth-user.decorator';
-import { CommandBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateAssetDto } from '@/modules/asset/dto/create-asset.dto';
 import { AuthGuard } from '@/modules/auth/auth.guard';
-import { CreateAssetCommand } from '@/modules/asset/command/create-asset.command';
+import { CreateAssetCommand } from '@/modules/asset/commands/create-asset.command';
 import { AssetEntity } from '@/modules/asset/entities/asset.entity';
+import { GetAssetByIdQuery } from '@/modules/asset/queries/get-asset-by-id.query';
 
 @Controller('assets')
 export class AssetController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @ApiBody({ type: CreateTextTemplateDto })
   @ApiResponse({
@@ -40,5 +45,22 @@ export class AssetController {
         user,
       }),
     );
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: 'number',
+    description: 'The ID of the asset to get',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Asset retrieved successfully',
+  })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @Get('/:id')
+  @UseGuards(AuthGuard)
+  async get(@Param('id') id: number) {
+    return this.queryBus.execute(new GetAssetByIdQuery(id));
   }
 }
--- src/modules/asset/asset.module.ts
+++ src/modules/asset/asset.module.ts
@@ -3,9 +3,10 @@ import { AssetController } from './asset.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssetEntity } from '@/modules/asset/entities/asset.entity';
 import { CqrsModule } from '@nestjs/cqrs';
-import { CreateAssetCommandHandler } from '@/modules/asset/command/create-asset.command';
+import { CreateAssetCommandHandler } from '@/modules/asset/commands/create-asset.command';
 import { EcomPolicyEntity } from '@/modules/asset/entities/ecom-policy.entity';
 import { EcomOrderEntity } from '@/modules/asset/entities/ecom-order.entity';
+import { GetAssetByIdQueryHandler } from '@/modules/asset/queries/get-asset-by-id.query';
 
 @Module({
   imports: [
@@ -13,6 +14,6 @@ import { EcomOrderEntity } from '@/modules/asset/entities/ecom-order.entity';
     CqrsModule,
   ],
   controllers: [AssetController],
-  providers: [CreateAssetCommandHandler],
+  providers: [CreateAssetCommandHandler, GetAssetByIdQueryHandler],
 })
 export class AssetModule {}
