c2fd8151d735031f6d1b21a3ff9dd6641792a4ad
Dean Gite <dean.gite@cigen.me>
2023-10-02 14:17:45 +0000

    Merged PR 660: update create user id
    Related work items: #1492

--- src/modules/asset/command/create-asset.command.ts
+++ src/modules/asset/command/create-asset.command.ts
@@ -1,7 +1,6 @@
 import { CommandHandler, ICommand, ICommandHandler } from '@nestjs/cqrs';
-import { InjectRepository } from '@nestjs/typeorm';
 import { AssetEntity } from '@/modules/asset/entities/asset.entity';
-import { Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { EcomPolicyEntity } from '@/modules/asset/entities/ecom-policy.entity';
 import { EcomOrderEntity } from '@/modules/asset/entities/ecom-order.entity';
 
@@ -32,62 +31,64 @@ export class CreateAssetCommand implements ICommand {
 export class CreateAssetCommandHandler
   implements ICommandHandler<CreateAssetCommand>
 {
-  constructor(
-    @InjectRepository(AssetEntity)
-    public readonly assetRepository: Repository<AssetEntity>,
-    @InjectRepository(EcomPolicyEntity)
-    public readonly ecomPolicyRepository: Repository<EcomPolicyEntity>,
-    @InjectRepository(EcomOrderEntity)
-    public readonly ecomOrderEntityRepository: Repository<EcomOrderEntity>,
-  ) {}
+  constructor(readonly dataSource: DataSource) {}
 
   async execute({ payload }: CreateAssetCommand) {
-    const ecomPolicy = await this.ecomPolicyRepository
-      .createQueryBuilder('ecomPolicy')
-      .where('ecomPolicy.customerId = :customerId', {
-        customerId: payload.user.cid,
-      })
-      .orWhere('ecomPolicy.customerHeadId = :customerHeadId', {
-        customerHeadId: payload.user.chid,
-      })
-      .getOne();
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    try {
+      const ecomPolicy = await queryRunner.manager
+        .createQueryBuilder(EcomPolicyEntity, 'ecomPolicy')
+        .where('ecomPolicy.customerId = :customerId', {
+          customerId: payload.user.cid,
+        })
+        .orWhere('ecomPolicy.customerHeadId = :customerHeadId', {
+          customerHeadId: payload.user.chid,
+        })
+        .andWhere('ecomPolicy.isActive = :isActive', { isActive: 1 })
+        .getOne();
 
-    const assetCreateData: Partial<AssetEntity> = {
-      assetDescription: payload.assetDescription,
-      userTypeId: payload.userTypeId,
-      userTypeDescription: payload.userName,
-      statusId: payload.statusId,
-      ...(payload.ecomOrderId && { ecomOrderId: payload.ecomOrderId }),
-      ...(payload.orderId && { orderId: payload.orderId }),
-      ...(payload.costObjectId && { costObjectId: payload.costObjectId }),
-      comment: payload.comment,
-      cost: payload.cost,
-      customerAddressId: payload.customerAddressId,
-      typeId: payload.typeId,
-      imeiSnr: payload.imeiSnr,
-      customerId: payload.customerId,
-      ownershipId: payload.ownershipTypeId,
-      createdDate: new Date(),
-      createdUserId: payload.user.id,
-    };
-
-    // there is no active ecom policy
-    if (!ecomPolicy) {
-      const ecomOrderCreate = this.ecomOrderEntityRepository.create({
-        orderDate: new Date(),
-        policyId: payload.ecomPolicyId,
-        costObjectId: payload.costObjectId,
+      const assetCreateData: Partial<AssetEntity> = {
+        assetDescription: payload.assetDescription,
+        userTypeId: payload.userTypeId,
+        userTypeDescription: payload.userName,
+        statusId: payload.statusId,
+        ...(payload.ecomOrderId && { ecomOrderId: payload.ecomOrderId }),
+        ...(payload.orderId && { orderId: payload.orderId }),
+        ...(payload.costObjectId && { costObjectId: payload.costObjectId }),
+        comment: payload.comment,
+        cost: payload.cost,
+        customerAddressId: payload.customerAddressId,
+        typeId: payload.typeId,
+        imeiSnr: payload.imeiSnr,
         customerId: payload.customerId,
-        coverAmount: payload.cost,
-        status: 10, // migration status
-      });
-      const ecomOrder = await this.ecomOrderEntityRepository.save(
-        ecomOrderCreate,
-      );
-      assetCreateData.ecomOrderId = ecomOrder.id;
+        ownershipId: payload.ownershipTypeId,
+        createdDate: new Date(),
+        createdUserId: payload.user.uid,
+      };
+      await queryRunner.startTransaction();
+      // there is no active ecom policy
+      if (ecomPolicy && payload.ecomPolicyId) {
+        const ecomOrderCreate = queryRunner.manager.create(EcomOrderEntity, {
+          orderDate: new Date(),
+          policyId: payload.ecomPolicyId,
+          costObjectId: payload.costObjectId,
+          customerId: payload.customerId,
+          coverAmount: payload.cost,
+          status: 10, // migration status
+        });
+        const ecomOrder = await queryRunner.manager.save(ecomOrderCreate);
+        assetCreateData.ecomOrderId = ecomOrder.id;
+      }
+      const newAsset = queryRunner.manager.create(AssetEntity, assetCreateData);
+      const savedAsset = await queryRunner.manager.save(newAsset);
+      await queryRunner.commitTransaction();
+      return savedAsset;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
-
-    const newAsset = this.assetRepository.create(assetCreateData);
-    return this.assetRepository.save(newAsset);
   }
 }
