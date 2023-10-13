c1d8b066498beff47de14e2f18079661195bf06d
Dean Gite <dean.gite@cigen.me>
2023-10-13 14:01:28 +0000

    Merged PR 689: update asset
    Related work items: #1492

--- src/modules/asset/commands/create-asset.command.ts
+++ src/modules/asset/commands/create-asset.command.ts
@@ -62,9 +62,11 @@ export class CreateAssetCommandHandler
         typeId: payload.typeId,
         imeiSnr: payload.imeiSnr,
         customerId: payload.customerId,
-        ownershipId: payload.ownershipTypeId,
+        ownershipId: 1,
         createdDate: new Date(),
         createdUserId: payload.user.uid,
+        ...(payload.ownershipTypeId <= 0 && { leasingVendorId: null }),
+        ...(payload.ownershipTypeId > 1 && { isLeased: true }),
       };
       await queryRunner.startTransaction();
       // there is no active ecom policy
