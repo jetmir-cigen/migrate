010cd401c93af43ebe7c0a5f30ccfd883ad29b43
Jetmir Avdullahu <jetmir.avdullahu@gmail.com>
2024-03-18 14:13:20 +0100

    move project to apps

--- apps/manager-api/src/main.ts
+++ apps/manager-api/src/main.ts
@@ -1,20 +1,53 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
-}
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.use(cookieParser());
+  app.setGlobalPrefix('api/v1');
+  // Enable cors, so FE can access it.
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000', /.+vercel\.app/, /.+skytechcontrol\.io/],
+    allowedHeaders: [
+      'authorization',
+      'cookie',
+      'cookies',
+      'content-type',
+      'baggage',
+      'sentry-trace',
+    ],
+  });
 
+  //For custom validations
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Manager API')
+    .setDescription('The manager API description')
+    .setVersion('1.0')
+    .addTag('manager')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const PORT = process.env.PORT || 4000;
+
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 bootstrap();
